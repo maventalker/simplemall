04594d2465b5a672f1d6a615174335dbfc90aa68
xingxing <xingxing@192.168.0.104>
2017-08-13 07:41:58 +0800

    fullwith payservice

--- payment-service/src/main/java/com/simplemall/micro/serv/pay/controller/PayController.java
+++ payment-service/src/main/java/com/simplemall/micro/serv/pay/controller/PayController.java
@@ -2,9 +2,11 @@ package com.simplemall.micro.serv.pay.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
 import com.simplemall.micro.serv.pay.service.IPayService;
 
 @RestController
@@ -15,10 +17,13 @@ public class PayController {
 	IPayService payService;
 	
 	/**
+	 * pay 
+	 * 
 	 * @param serialNo
 	 * @param payType
 	 * @return
 	 */
+	@RequestMapping(value = "/pay",method = RequestMethod.POST)
 	public String pay(@RequestParam("serialNo") String serialNo,@RequestParam("payType") String payType ){
 		return null;
 	}
--- payment-service/src/main/java/com/simplemall/micro/serv/pay/service/IPayService.java
+++ payment-service/src/main/java/com/simplemall/micro/serv/pay/service/IPayService.java
@@ -1,5 +1,14 @@
 package com.simplemall.micro.serv.pay.service;
 
+import java.math.BigDecimal;
+
 public interface IPayService {
 
+	/**
+	 * @param serialNo
+	 * @param payType
+	 * @param price
+	 * @return
+	 */
+	int pay(String serialNo,String payType,BigDecimal price);
 }
