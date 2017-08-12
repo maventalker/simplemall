26f48ddfb4c007d881f3f35cf34f0c1df881e69b
xingxing <xingxing@192.168.0.104>
2017-08-13 07:09:28 +0800

    update order service

--- order-service/src/main/java/com/simplemall/micro/serv/order/OrderServApplication.java
+++ order-service/src/main/java/com/simplemall/micro/serv/order/OrderServApplication.java
@@ -1,5 +1,14 @@
 package com.simplemall.micro.serv.order;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
 public class OrderServApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServApplication.class, args);
+	}
 }
