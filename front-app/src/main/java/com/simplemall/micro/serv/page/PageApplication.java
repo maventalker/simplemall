e36120985251e0ce7708c33cdbc01c4e672b76ea
xingxing <xingxing@192.168.0.104>
2017-08-13 07:49:30 +0800

    add frontpage application

---
+++ front-app/src/main/java/com/simplemall/micro/serv/page/PageApplication.java
@@ -0,0 +1,14 @@
+package com.simplemall.micro.serv.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageApplication.class, args);
+	}
+
+}
---
+++ front-app/src/main/java/com/simplemall/micro/serv/page/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author xingxing
+ *
+ */
+package com.simplemall.micro.serv.page;
