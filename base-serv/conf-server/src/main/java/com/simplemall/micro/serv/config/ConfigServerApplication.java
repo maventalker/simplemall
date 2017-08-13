f9ab59b594348f86071dec57eb6e06344208ed0c
xingxing <xingxing@192.168.0.104>
2017-08-13 08:29:12 +0800

    update baseServ configuration

---
+++ base-serv/conf-server/src/main/java/com/simplemall/micro/serv/config/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.simplemall.micro.serv.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
---
+++ base-serv/eureka-server/src/main/java/com/simplemall/micro/serv/eureka/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.simplemall.micro.serv.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
