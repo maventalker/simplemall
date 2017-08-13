13b2b7af543bcedbfed9318d4c75c2c65fecfcc7
xingxing <xingxing@192.168.0.104>
2017-08-13 08:31:15 +0800

    update productServ configuration

---
+++ product-service/src/main/java/com/simplemall/micro/serv/prd/ProductServApplication.java
@@ -0,0 +1,14 @@
+package com.simplemall.micro.serv.prd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProductServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServApplication.class, args);
+	}
+
+}
