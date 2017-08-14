ffdfb4e1e3de1629b3283587067d35095bb56770
xingxing <xingxing@192.168.10.152>
2017-08-14 21:31:50 +0800

    update account service config

--- account-sevice/src/main/java/com/simplemall/micro/serv/account/AccountApplication.java
+++ account-sevice/src/main/java/com/simplemall/micro/serv/account/AccountApplication.java
@@ -2,9 +2,11 @@ package com.simplemall.micro.serv.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountApplication {
 
 	public static void main(String[] args) {
