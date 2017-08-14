acf57e35e5818701c973734064575eb58678655c
xingxing <xingxing@192.168.10.152>
2017-08-14 21:44:16 +0800

    enable payment/product eureka server

--- product-service/src/main/java/com/simplemall/micro/serv/prd/ProductServApplication.java
+++ product-service/src/main/java/com/simplemall/micro/serv/prd/ProductServApplication.java
@@ -2,8 +2,9 @@ package com.simplemall.micro.serv.prd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductServApplication {
 
