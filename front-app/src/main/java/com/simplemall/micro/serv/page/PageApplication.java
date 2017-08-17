f647724e80e75a73f88516e09d80d5b08f0a56c4
xingxing <xingxing@192.168.0.101>
2017-08-17 21:54:03 +0800

    add other shopping files

--- front-app/src/main/java/com/simplemall/micro/serv/page/PageApplication.java
+++ front-app/src/main/java/com/simplemall/micro/serv/page/PageApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 
 @ComponentScan
 @EnableFeignClients
@@ -21,6 +25,7 @@ public class PageApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
@@ -30,9 +35,9 @@ public class PageApplication {
 	 * 
 	 * @return
 	 */
-	/*@Bean
+	@Bean
 	public IRule ribbonRule() {
 		return new RandomRule();
-	}*/
+	}
 
 }
--- front-app/src/main/java/com/simplemall/micro/serv/page/api/APIProductController.java
+++ front-app/src/main/java/com/simplemall/micro/serv/page/api/APIProductController.java
@@ -3,14 +3,14 @@ package com.simplemall.micro.serv.page.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.simplemall.micro.serv.common.bean.RestAPIResult;
+import com.simplemall.micro.serv.common.bean.product.PrdInfo;
+import com.simplemall.micro.serv.page.client.ProductFeignClient;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,19 +24,15 @@ public class APIProductController {
 	private Logger logger = LoggerFactory.getLogger(APIProductController.class);
 
 	@Autowired
-	RestTemplate restTemplate;
+	ProductFeignClient feignClient;
 
-	@Autowired
-	LoadBalancerClient loandBalancerClient;
-
-	@ApiOperation(value = "get product detail info")
+	@ApiOperation(value = "get product detail")
 	@RequestMapping(value = "{tid}", method = RequestMethod.POST)
-	public RestAPIResult<String> getProductById(
+	public RestAPIResult<PrdInfo> getProductById(
 			@ApiParam(value = "product id") @RequestParam(required = true) String prdId) {
-		this.loandBalancerClient.choose("product-service");
 		logger.info("begin invoke product service");
-		restTemplate.getForEntity("", String.class).getBody();
-		return null;
+		RestAPIResult<PrdInfo> restAPIResult = feignClient.getPorudctById(prdId);
+		return restAPIResult;
 	}
 
 }
