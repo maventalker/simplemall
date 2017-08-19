880a11c88885eb4d6b088f160b1125e71d8ffde6
guodeqiang <guodeqiang@moneytocar.com>
2017-08-19 11:48:21 +0800

    update systemconstants

--- common-module/src/main/java/com/simplemall/micro/serv/common/constant/SystemConstants.java
+++ common-module/src/main/java/com/simplemall/micro/serv/common/constant/SystemConstants.java
@@ -10,7 +10,7 @@ public interface SystemConstants {
 
 	public static final class Code {
 		public static final String SUCCESS = "1";
-		
+
 		public static final String FAIL = "0";
 		/**
 		 * @Fields ok : 成功
@@ -20,5 +20,34 @@ public interface SystemConstants {
 		 * @Fields error : 失败
 		 */
 		public static final int error = 0;
+	}
+
+	/**
+	 * 订单状态
+	 * 
+	 * @author guooo
+	 *
+	 */
+	public static final class STATE {
+
+		/**
+		 * 创建
+		 */
+		public static final String CREATE = "CREATE";
+
+		/**
+		 * 配送中
+		 */
+		public static final String SHIPPING = "SHIPPING";
+
+		/**
+		 * 关闭
+		 */
+		public static final String CLOSED = "CLOSED";
+
+		/**
+		 * 完成
+		 */
+		public static final String FINISHED = "FINISHED";
 	}
 }
