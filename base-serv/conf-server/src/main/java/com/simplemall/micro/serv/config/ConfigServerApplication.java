a6fdc37702b464e9be687bfd48990edcf0940d3b
guodeqiang <guodeqiang@moneytocar.com>
2017-08-24 07:40:14 +0800

    add spring config supported

--- base-serv/conf-server/src/main/java/com/simplemall/micro/serv/config/ConfigServerApplication.java
+++ base-serv/conf-server/src/main/java/com/simplemall/micro/serv/config/ConfigServerApplication.java
@@ -2,9 +2,11 @@ package com.simplemall.micro.serv.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
