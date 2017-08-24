576f8d69e58a8faaf557506794e388edf950205d
guodeqiang <guodeqiang@moneytocar.com>
2017-08-25 06:54:27 +0800

    enforce config service/client to link config-service must config inside bootstrap properties

--- base-serv/conf-server/src/main/java/com/simplemall/micro/serv/config/ConfigServerApplication.java
+++ base-serv/conf-server/src/main/java/com/simplemall/micro/serv/config/ConfigServerApplication.java
@@ -2,13 +2,21 @@ package com.simplemall.micro.serv.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-
+/**
+ * spring config server
+ * 
+ * @author guooo
+ *
+ */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
-
+	
+	// FIXME encrypt配置文件
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
