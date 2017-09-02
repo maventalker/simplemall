f398d4d0b7cfee6b52175abaf29e89dc7c9b3800
xingxing <xingxing@192.168.0.103>
2017-09-03 07:01:02 +0800

    add oauth moudle

---
+++ base-serv/oauth-server/src/main/java/com/simplemall/micro/serv/base/oauth2/OAuth2ServApplication.java
@@ -0,0 +1,15 @@
+package com.simplemall.micro.serv.base.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Guooo 2017年9月3日 上午7:00:42
+ */
+@SpringBootApplication
+public class OAuth2ServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2ServApplication.class, args);
+	}
+}
