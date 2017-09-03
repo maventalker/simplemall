45fff9119030a56d757af626721752b0cb1b3e94
xingxing <xingxing@192.168.0.100>
2017-09-03 21:36:31 +0800

    add hystrix base support

--- account-sevice/src/main/java/com/simplemall/account/AccountServApplication.java
+++ account-sevice/src/main/java/com/simplemall/account/AccountServApplication.java
@@ -3,6 +3,7 @@ package com.simplemall.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * 启动类
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class AccountServApplication {
 
 	public static void main(String[] args) {
