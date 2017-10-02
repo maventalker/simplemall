38738ccbe69fb01f08865b91aab108ec4396a8ef
guodeqiang <guodeqiang@moneytocar.com>
2017-10-02 16:13:50 +0800

    update zuul server autoconfig use eureka client

--- base-serv/zuul-server/src/main/java/com/simplemall/micro/serv/zuul/ZuulServerApplication.java
+++ base-serv/zuul-server/src/main/java/com/simplemall/micro/serv/zuul/ZuulServerApplication.java
@@ -2,8 +2,17 @@ package com.simplemall.micro.serv.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
+/**
+ * API Gateway
+ * 
+ * @author guooo
+ *
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulServerApplication {
 
