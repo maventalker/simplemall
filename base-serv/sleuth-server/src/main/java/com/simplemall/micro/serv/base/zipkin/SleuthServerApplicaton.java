82e8a64f0248994de41f483e0b39073907482640
xingxing <xingxing@192.168.2.1>
2017-10-05 07:46:27 +0800

    import sleuth middle used mq way

--- base-serv/sleuth-server/src/main/java/com/simplemall/micro/serv/base/zipkin/SleuthServerApplicaton.java
+++ base-serv/sleuth-server/src/main/java/com/simplemall/micro/serv/base/zipkin/SleuthServerApplicaton.java
@@ -2,11 +2,22 @@ package com.simplemall.micro.serv.base.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-import zipkin.server.EnableZipkinServer;
-
-@SpringBootApplication
-@EnableZipkinServer
+/**
+ * 两种方式实现链式跟踪，一种使用http的方式接受消息，一种采用消息中间件的形式来跟踪信息
+ * 下方注释掉的注解为http方式，与Pom.xml配置中io.zipkin.java/zipkin-server配套使用
+ * 目前代码采用的是mq异步消息方式跟踪信息
+ * 
+ * @author Guooo 2017年10月5日 上午6:55:59 Cannot determine embedded database driver
+ *         class for database type NONE
+ */
+//@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+// @EnableZipkinServer //使用Stream方式启动ZipkinServer
+@EnableZipkinStreamServer
 public class SleuthServerApplicaton {
 
 	public static void main(String[] args) {
--- base-serv/turbine-server/src/main/java/com/simplemall/micro/serv/base/turbine/TurbineApplication.java
+++ base-serv/turbine-server/src/main/java/com/simplemall/micro/serv/base/turbine/TurbineApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * 集群监控，必须引入acutor组件。 
+ * 
+ * TODO 可以依赖turbine-amqp，由rabbitmq作数据中转
+ * 
+ * @author Guooo 2017年10月3日 上午10:22:53
+ */
 @SpringBootApplication
 @EnableTurbine
 @EnableDiscoveryClient
